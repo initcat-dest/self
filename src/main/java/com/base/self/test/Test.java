c18923ca12f446b46d2d261e94347ad77edd0e3a
libo <libo@xmiles.cn>
2019-03-09 22:50:54 +0800

    添加内容

--- src/main/java/com/base/self/test/Test.java
+++ src/main/java/com/base/self/test/Test.java
@@ -1,8 +1,9 @@
 package com.base.self.test;
 
-import com.base.self.utils.UtilNumber;
+import org.apache.commons.lang3.time.DateUtils;
 
-import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.Date;
 
 public class Test {
 	public static void func(int a) {
@@ -10,59 +11,80 @@ public class Test {
 		System.out.println(a);
 	}
 
-	public static void main(String[] args) throws Exception {
-		// int a = 10;// 变量
-		// func(a);
-		// System.out.println(a);
-		//
-		// System.out.println(DateFormatUtils.format(1485193638L * 1000, "yyyy-MM-dd
-		// HH:mm:ss"));
-		//
-		// Float averageAward = 0.00f;
-		// if (averageAward != null && averageAward == 0) {
-		// System.out.println(111);
-		// }
-		// System.out.println(222);
+	//	public static void main(String[] args) throws Exception {
+//		// int a = 10;// 变量
+//		// func(a);
+//		// System.out.println(a);
+//		//
+//		// System.out.println(DateFormatUtils.format(1485193638L * 1000, "yyyy-MM-dd
+//		// HH:mm:ss"));
+//		//
+//		// Float averageAward = 0.00f;
+//		// if (averageAward != null && averageAward == 0) {
+//		// System.out.println(111);
+//		// }
+//		// System.out.println(222);
+//
+//		// BigDecimal money = new BigDecimal(0.7);
+//		// BigDecimal customsPassAwardIntervalStart = new BigDecimal(0.1);
+//		// BigDecimal customsPassAwardIntervalEnd = new BigDecimal(0.5);
+//		// if (money.compareTo(customsPassAwardIntervalStart) < 0 ||
+//		// money.compareTo(customsPassAwardIntervalEnd) > 0) {
+//		// for (int i = 0; i < 50; i++) {
+//		// money = new BigDecimal(Math.random()
+//		// * (customsPassAwardIntervalEnd.floatValue() -
+//		// customsPassAwardIntervalStart.floatValue())
+//		// + customsPassAwardIntervalStart.floatValue());
+//		// money = UtilNumber.transBase(money.floatValue(), 2, BigDecimal.ROUND_DOWN);
+//		// System.out.println(money);
+//		// }
+//		// }
+//		BigDecimal money = new BigDecimal(0); // 单用户奖金
+//		Float averageAward = 0.1f;
+//		// 如果配置人均奖金，则直接使用
+//		if (averageAward != null && averageAward > 0) {
+//			money = UtilNumber.transBase(averageAward, 2, BigDecimal.ROUND_DOWN);
+//			System.out.println(1);
+//		} else {
+//			// 奖金平均分
+//			money = UtilNumber.transBase(10 / 13413, 2, BigDecimal.ROUND_DOWN);
+//			BigDecimal awardMin = new BigDecimal(1.9);
+//			BigDecimal awardMax = new BigDecimal(1.9);
+//			if (awardMin != null && awardMax != null
+//					&& (money.compareTo(awardMin) < 0 || money.compareTo(awardMax) > 0)) {
+//				money = new BigDecimal(
+//						Math.random() * (awardMax.floatValue() - awardMin.floatValue()) + awardMin.floatValue());
+//				money = UtilNumber.transBase(money.floatValue(), 2, BigDecimal.ROUND_DOWN);
+//				System.out.println(2);
+//			}
+//			if ((awardMin == null || awardMax == null)
+//					&& (money.compareTo(new BigDecimal(0.1)) < 0 || money.compareTo(new BigDecimal(0.5)) > 0)) {
+//				// 若奖励金额小于0.1 或 大于0.5 取0.1到0.5的随机值
+//				money = UtilNumber.transBase(((float) (1 + Math.random() * 4) / 10), 2, BigDecimal.ROUND_DOWN);
+//				System.out.println(3);
+//			}
+//		}
+//		System.out.println(money);
+//	}
 
-		// BigDecimal money = new BigDecimal(0.7);
-		// BigDecimal customsPassAwardIntervalStart = new BigDecimal(0.1);
-		// BigDecimal customsPassAwardIntervalEnd = new BigDecimal(0.5);
-		// if (money.compareTo(customsPassAwardIntervalStart) < 0 ||
-		// money.compareTo(customsPassAwardIntervalEnd) > 0) {
-		// for (int i = 0; i < 50; i++) {
-		// money = new BigDecimal(Math.random()
-		// * (customsPassAwardIntervalEnd.floatValue() -
-		// customsPassAwardIntervalStart.floatValue())
-		// + customsPassAwardIntervalStart.floatValue());
-		// money = UtilNumber.transBase(money.floatValue(), 2, BigDecimal.ROUND_DOWN);
-		// System.out.println(money);
-		// }
-		// }
-		BigDecimal money = new BigDecimal(0); // 单用户奖金
-		Float averageAward = 0.1f;
-		// 如果配置人均奖金，则直接使用
-		if (averageAward != null && averageAward > 0) {
-			money = UtilNumber.transBase(averageAward, 2, BigDecimal.ROUND_DOWN);
-			System.out.println(1);
-		} else {
-			// 奖金平均分
-			money = UtilNumber.transBase(10 / 13413, 2, BigDecimal.ROUND_DOWN);
-			BigDecimal awardMin = new BigDecimal(1.9);
-			BigDecimal awardMax = new BigDecimal(1.9);
-			if (awardMin != null && awardMax != null
-					&& (money.compareTo(awardMin) < 0 || money.compareTo(awardMax) > 0)) {
-				money = new BigDecimal(
-						Math.random() * (awardMax.floatValue() - awardMin.floatValue()) + awardMin.floatValue());
-				money = UtilNumber.transBase(money.floatValue(), 2, BigDecimal.ROUND_DOWN);
-				System.out.println(2);
-			}
-			if ((awardMin == null || awardMax == null)
-					&& (money.compareTo(new BigDecimal(0.1)) < 0 || money.compareTo(new BigDecimal(0.5)) > 0)) {
-				// 若奖励金额小于0.1 或 大于0.5 取0.1到0.5的随机值
-				money = UtilNumber.transBase(((float) (1 + Math.random() * 4) / 10), 2, BigDecimal.ROUND_DOWN);
-				System.out.println(3);
+	public static void main(String[] args) throws ParseException {
+		Date date = DateUtils.parseDate("2019-11-25", "yyyy-MM-dd");
+
+		System.out.println("date.getMonth():" + date.getMonth());
+		System.out.println("date.getDate():"+date.getDate());
+		System.err.println(date.getMonth() + "" + date.getDate());
+	}
+
+
+
+	public String method(String[] strs) {
+		StringBuffer sb = new StringBuffer();
+		if (strs != null) {
+			for (String str : strs) {
+				sb.append(str);
 			}
 		}
-		System.out.println(money);
+		return sb.toString();
 	}
+
 }
---
+++ src/main/java/com/base/self/thread/BadSuspend.java
@@ -0,0 +1,47 @@
+package com.base.self.thread;
+
+/**
+ * 线程的挂起方法
+ * 这里讨论的是过时的技术，仅为了测试使用jstack
+ *
+ * @author libo
+ * @package com.base.self.thread
+ * @company xmiles
+ * @date 2019/3/5
+ */
+public class BadSuspend {
+
+    public static Object u = new Object();
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    public static class ChangeObjectThread extends Thread {
+        public ChangeObjectThread(String name) {
+            super.setName(name);
+        }
+
+        @Override
+        public void run() {
+            synchronized (u) {
+                System.out.println("in " + getName());
+                // 挂起当前线程，并持有synchronized锁
+                Thread.currentThread().suspend();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 启动t1线程
+        t1.start();
+        Thread.sleep(100);
+        // 由于线程t1被挂起，t2.start()处于等待synchronized锁
+        t2.start();
+        // t1执行继续执行指令
+        t1.resume();
+        // t2.resume执行先于t2.suspend(),所以导致死锁
+        t2.resume();
+        t1.join();
+        t2.join();
+        // 在 Terminal中执行jstack pid(pid:线程ID)
+    }
+}
