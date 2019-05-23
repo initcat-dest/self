e0dd8d5b6d822ecb0fe28b0f813eaa3a2c258425
libo <libo@xmiles.cn>
2019-05-23 23:06:46 +0800

    爬虫

---
+++ src/main/java/com/base/self/booleanTest/BooleanTest.java
@@ -0,0 +1,33 @@
+package com.base.self.booleanTest;
+
+/**
+ * class description
+ *
+ * @author libo
+ * @package com.base.self.booleanTest
+ * @company xmiles
+ * @date 2019/5/21
+ */
+public class BooleanTest {
+    public static void main(String[] args) {
+        Boolean b1 = true;    //创建Boolean对象
+
+        Boolean b2 = false;  //创建Boolean对象
+
+        Boolean b3 = true;    //创建Boolean对象
+
+        int i;
+
+        i = b1.compareTo(b2);    //b1和b2进行比较
+
+        System.out.println(i);
+
+        i = b2.compareTo(b1);    //b2和b1进行比较
+
+        System.out.println(i);
+
+        i = b1.compareTo(b3);    //b1和b3进行比较
+
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/base/self/crawler/package-info.java
@@ -0,0 +1,4 @@
+package com.base.self.crawler;
+/**
+ * 爬虫
+ */
